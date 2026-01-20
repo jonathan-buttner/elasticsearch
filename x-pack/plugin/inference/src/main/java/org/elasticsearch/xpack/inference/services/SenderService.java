7e759b65616f97b4a8d7144e48655f83a39586bb
Jonathan Buttner <jonathan.buttner@elastic.co>
2026-01-20 17:04:05 -0500

    Example of removing factory

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/SenderService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/SenderService.java
@@ -43,6 +43,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import static org.elasticsearch.inference.InferenceStringGroup.indexContainingMultipleInferenceStrings;
+import static org.elasticsearch.xpack.inference.services.ServiceUtils.createInvalidTaskTypeException;
 import static org.elasticsearch.xpack.inference.services.ServiceUtils.throwUnsupportedEmbeddingOperation;
 
 public abstract class SenderService implements InferenceService {
@@ -50,6 +51,31 @@ public abstract class SenderService implements InferenceService {
     private final Sender sender;
     private final ServiceComponents serviceComponents;
     private final ClusterService clusterService;
+
+    /**
+     * Retrieves a {@link ModelCreator} from the provided map based on the task type, or throws an exception if not found.
+     * @param modelCreators the map of task types to model creators
+     * @param inferenceId the inference entity ID
+     * @param taskType the task type
+     * @param service the service name
+     * @param context the configuration parse context
+     * @param <C> the type of {@link ModelCreator}
+     * @return the retrieved {@link ModelCreator}
+     * @throws ElasticsearchStatusException if no {@link ModelCreator} is found for the given task type
+     */
+    public static <C extends ModelCreator<? extends Model>> C retrieveModelCreatorFromMapOrThrow(
+        Map<TaskType, C> modelCreators,
+        String inferenceId,
+        TaskType taskType,
+        String service,
+        ConfigurationParseContext context
+    ) {
+        C modelCreator = modelCreators.get(taskType);
+        if (modelCreator == null) {
+            throw createInvalidTaskTypeException(inferenceId, service, taskType, context);
+        }
+        return modelCreator;
+    }
 
     public SenderService(HttpRequestSender.Factory factory, ServiceComponents serviceComponents, ClusterService clusterService) {
         Objects.requireNonNull(factory);
